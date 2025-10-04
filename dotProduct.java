276d3069d4163fffb07de5611d6aa377be955e7a
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-10-04 10:22:59 -0400

    finshed the class

--- dotProduct.java
+++ dotProduct.java
@@ -1,30 +1,32 @@
-class dotProduct{
+class DotProduct{
     // (2 * 1) + (3 * 5) + (2 * 2)
     // [2 3 2] * [1 5 2]    same thing should be implemnt
     int[] a;
     int[] b;
 
-    public dotProduct(){}
+    public DotProduct(){}
 
-    public dotProduct(int[] a, int[] b){
+    public DotProduct(int[] a, int[] b){
         this.a = a; this.b = b;
     }
 
-    public static int totalSum(int[] a, int[] b){
+    // getting the total sum
+    public int totalSum(int[] a, int[] b){
         int sum = 0;
 
-        for (int i = 0; i <= a.length(); i++){
+        for (int i = 0; i <= a.length; i++){
             sum += (a[i] * b[i]);
         }
         return(sum);
     }
 
-    public void setAList(int[] newA){
-        a = newA;
+    // sets new list
+    // made it where they have the same length
+    public void setArrays(int[] newA, int[] newB){
+        if (newA.length != newB.length){
+            throw new IllegalArgumentException("Arrays must have the same lenght");
+        }
+        this.a = newA; this.b = newB;
     }
-    public void setBList(int[] newB){
-        b = newB;
-    }
-    
 }
 
