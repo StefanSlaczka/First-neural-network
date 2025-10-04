30293cc3bb218eb850b10b3cebb5b6d519e833cd
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-10-04 10:15:01 -0400

    hand coded

--- dotProduct.java
+++ dotProduct.java
@@ -1,3 +1,30 @@
 class dotProduct{
+    // (2 * 1) + (3 * 5) + (2 * 2)
+    // [2 3 2] * [1 5 2]    same thing should be implemnt
+    int[] a;
+    int[] b;
+
+    public dotProduct(){}
+
+    public dotProduct(int[] a, int[] b){
+        this.a = a; this.b = b;
+    }
+
+    public static int totalSum(int[] a, int[] b){
+        int sum = 0;
+
+        for (int i = 0; i <= a.length(); i++){
+            sum += (a[i] * b[i]);
+        }
+        return(sum);
+    }
+
+    public void setAList(int[] newA){
+        a = newA;
+    }
+    public void setBList(int[] newB){
+        b = newB;
+    }
     
 }
+
