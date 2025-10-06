7c9e4d3638102c4cb6bcd0323fddf7f0886812c3
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-10-06 11:09:54 -0400

    deritvies

---
+++ derivatives/FindDerivative.java
@@ -0,0 +1,11 @@
+/*
+    y=x**2
+    => 2x**2 // by slipping down the exponent x**2 the 2 down infront of and multiply it agiast the base
+    => 2x**1 // subtract one from the exponent
+    => 2x
+
+    y=3x**2
+    => (3 * 2)x**2
+    => 6x**(2-1)
+    => 6x
+*/
