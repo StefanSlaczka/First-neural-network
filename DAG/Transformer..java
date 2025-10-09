ae249aaec52349c0a4c1c3e0a572940535237c86
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-10-09 18:01:49 -0400

    hardcoding somthing simple to undersatnd it better

---
+++ DAG/Transformer..java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+// every conection (edge) has a direction from input to output
+// infomation flows forward and no loops
+
+class Transformer{
+    /*
+    Embedding layer → turns tokens (“hello”) into vectors.
+
+    Self-Attention layer → each token looks at the others to understand context.
+
+    Feedforward layer → processes each position’s information further.
+
+    Residual + Normalization → adds shortcuts and stabilizes learning.
+    */
+
+   // input hello -> [h,e,l,l,o]
+
+   Scanner scanner = new Scanner(system.in);
+
+
+}
--- derivatives/FindDerivative.java
+++ derivatives/FindDerivative.java
@@ -1,49 +1,23 @@
-/*
-    // This helps us to inhanses the ai to find the slope
-
-    y=x**2
-    => 2x**2 // by slipping down the exponent x**2 the 2 down infront of and multiply it agiast the base
-    => 2x**1 // subtract one from the exponent
-    => 2x
-
-    y=3x**2
-    => (3 * 2)x**2
-    => 6x**(2-1)
-    => 6x
-
-    y=2x ?
-    2x**1
-    2x**0
-    1*2 =2
-
-    any constant is 0
-    y=5?
-    => 0
-
-    y=x+5
-    x => 1 and 5 => 0
-    == 1
-
-    y= 2x**2-3x+8
-    2x**2 => 4x
-    -3x => -3
-    8 => 0
-    == 4x-3
-*/
-
-/*  // Multiplying 2 functions f(x) and g(x) to h(x)
-    h’(x) = f’(x)g(x) + f(x)g’(x)
+class FindDerivative{
+    String function = "6x**2";
+    String powered;
+    String insual;
+    char[] charArray;
     
-    f(x) = 3x, g(x) = 2x**2
-    f’x) = 3, g’(x) = 4x
+    public FindDerivative(){}
+    public FindDerivative(String function){
+        if (function.contains("**") == true) {
+            // powered = function[-1]
+            // insual = function[0]
 
-    h’(x) = 3(2x**2) + 3x(4x)
-    => 6x**2+12x**2 => 18x**2
+            charArray = function.toCharArray();
+            powered = function[-1];
+            insual = function[0];
 
-    f(x)*g(x) = 6x**3
-    // the derivative of 6x**2 is 18x**2
-*/
+            if (insual == "x"){
 
-/*
-
-*/
+            }
+        }
+    }
+    
+}
